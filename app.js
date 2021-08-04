1bc5c655c20fbeb31f446c02f63a6f41adba6821
quantumqyle <quantumqyle@mailbox.org>
2021-08-03 20:57:15 -0400

    nodeman update 1

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const path = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get ("", (req, res) => {
+    res.status(200).send("MEAN");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listenings to request from: https://localhost:${PORT}`);
+});
